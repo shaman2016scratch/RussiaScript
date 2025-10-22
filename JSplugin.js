17b3df8d4a89c9e32083b0ad7c100d43718c618b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-22 23:44:21 +0300

    Create JSplugin.js

---
+++ JSplugin.js
@@ -0,0 +1,6 @@
+function RussiaScriptRun(command, r) {
+  if (r == (true)) {
+    return ('RussiaScript (RS) is still in development')
+  }
+  console.log('RussiaScript (RS) is still in development')
+}
