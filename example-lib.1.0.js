24d17b16aa84e1bce17c2852259e72de133cacd0
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-24 13:13:56 +0300

    Update example-lib.1.0.js

--- example-lib.1.0.js
+++ example-lib.1.0.js
@@ -1,29 +1,29 @@
-class Lib
-  info = {
-    "created": "Example User",
-    "isDocs": "true",
-    "Docs": "example.com",
-    "id": "ExampleId",
-    "Name": "ExampleName",
-    "version": "1.0",
-    "isicon": "true",
-    "icon": "example.com/example.img",
-    "iscolor": "true",
-    "colors": "3",
-    "color1": "",
-    "color2": "",
-    "color3": ""
+info = {
+  "created": "Example User",
+  "isDocs": "true",
+  "Docs": "example.com",
+  "id": "ExampleId",
+  "Name": "Example Name",
+  "version": "1.0",
+  "isicon": "true",
+  "icon": "example.com/example.img",
+  "iscolor": "true",
+  "colors": "3",
+  "color1": "",
+  "color2": "",
+  "color3": ""
+}
+Blocks = [
+  {
+    "opcode": "block1",
+    "name": "example block",
+    "params": {},
+    "block type": "command",
   }
-  Functions = [
-    {
-      "opcode": "block1",
-      "name": "example block",
-      "params": {},
-      "block type": "command",
-    }
-  ]
-  block(opcode, args) {
-    if (opcode == 'block1') {
-      cosnole.log('example log!')
-    }
+]
+function block(opcode, args) {
+  if (opcode == 'block1') {
+    cosnole.log('example log!')
   }
+}
+ExtensionRussiaScript('ExampleId', '1.0', 'ExampleUser', 'Example Name', info, Blocks, 'block')
