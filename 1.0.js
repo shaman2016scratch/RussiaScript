7329b49a70398f05260f326095589d9844c0d103
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-24 13:59:07 +0300

    Update 1.0.js

--- 1.0.js
+++ 1.0.js
@@ -14,143 +14,6 @@ function ExtensionRussiaScript(IdLib, VersionLib, CreatedLib, NameLib, InfoLib, 
   }
   RussiaScriptLibs.push(LibData)
 }
-function SessionRussiaScript(RK) {
-  SessionRussiaScript = {
-    "ОС": "HarmonyOS",
-    "ИмяПрограммы": "RussiaScriptRinner",
-    "Язык": "Ru",
-    "Версия RussiaScript": "1.0",
-    "User-agent": "Mozilla/5.0"
-  }
-  SessionRS = JSON.stringify(SessionRussiaScript)
-  OperationSystems = [
-    "Unix",
-    "MacOS",
-    "BSD",
-    "Linux",
-    "Android",
-    "HarmonyOS",
-    "ios",
-    "ipadOS",
-    "tvOS"
-  ]
-  SessionRS.["ИмяПрограммы"] = RK.progname
-  SessionRS.["Язык"] = RK.lang
-  SessionRS.["Версия RussiaScript"] = "1.0"
-  UserAgent = fetch('https://httpbin.org/user-agent', {  
-        method: 'GET',  
-        headers: { 
-          "Content-Type": "application/json",  
-        }
-      })
-  SessionRS.["User-agent"] = UserAgent.["user-agent"]
-  OperationSystem = RK.os
-  OperationSystem = fetch(`https://shaman2016scratch.github.io/os?os="${OperationSystem}"`, {  
-        method: 'GET',  
-        headers: { 
-          "Content-Type": "application/json",  
-        }
-      })
-  SessionRS.["ОС"] = OperationSystem
-  SessionRussiaScript = JSON.parse(SessionRS)
-}
-function runRussiaScript(code) {
-  DataTypes = [
-    "text",
-    "math",
-    "boolean",
-    "peremens",
-    "block"
-  ]
-  RussiaScriptCommands = [
-    "Очистить Вывод",
-    "http запрос",
-    "Вывести",
-    "json",
-    "dom",
-    "Задать переменной значение",
-    "Импортировать расширение",
-    "Получить значение переменной"
-  ]
-  LibsRussiaScript = code.libs
-  codeRussiaScript = code.code
-  RunKey = code.RunKey
-  SessionRussiaScript(RunKey)
-  Peremens = {}
-  i = 0
-  i2 = 'true'
-  while (i2) {
-    if (codeRussiaScript.length < i) {
-      i2 = (false)
-    } else {
-      i2 = (true)
-    }
-    i++
-    i3 = codeRussiaScript.[i]
-    i4 = i3.command
-    i5 = i4.params
-    if (i4 == 'Очистить Вывод') {
-      RussiaScriptOutput = []
-    }
-    if (i4 == 'http запрос') {
-      HttpZapros = [
-        "Post",
-        "Delete",
-        "Get"
-      ]
-      fetch(i5.url, i5.params)
-    }
-    if (i4 == 'Вывести') {
-      RussiaScriptOutput.push(RussiaScriptGetValue(i5["Текст вывода"]))
-    }
-    if (i4 == 'json') {
-      RSjsonData = {
-        "Operations": [
-          "в Json-строку",
-          "в JavaScript обьект",
-          "Пустой объект",
-          "Пустой Массив",
-          "Задать ключ в объекте",
-          "Добавить элемент в начало массива",
-          "Добавить элемент в массив",
-          "Добавить элемент в массив в место",
-          "Удалить из объекта",
-          "Удалить последний элемент массива",
-          "Удалить первый элемент массива",
-          "Удалить элемент массива",
-          "Получить элемент массива",
-          "Получить элемент объекта"
-        ]
-      }
-    }
-    if (i4 == 'dom') {
-      RsDomData = {
-        "Operations": [
-          "getElement",
-          "innerHTML",
-          "TextContent",
-          "GetValue",
-          "NewElement"
-        ]
-      }
-    }
-    if (i4 == 'Задать переменной значение') {
-      peremens[i5.per] = i5.value
-    }
-    if (i4 == 'Импортировать расширение') {
-      console.error("RussiaScript Error: Import, Extensions (Libraries) not support!")
-    }
-  }
-}
-function RunRsCodeFromUrl(url) {
-  getCode = fetch(url, {  
-    method: 'GET',  
-    headers: { 
-      "Content-Type": "application/json",  
-    }
-  })
-  runRussiaScript(getCode)
-}
 function RussiaScriptGetValue(v) {
   ii = v.type
   ii2 = v.value
@@ -579,4 +442,141 @@ function RussiaScriptGetValue(v) {
   if (ii == 'число Эйлера') {
     return Math.E
   }
+}
+function SessionRussiaScript(RK) {
+  SessionRussiaScript = {
+    "ОС": "HarmonyOS",
+    "ИмяПрограммы": "RussiaScriptRinner",
+    "Язык": "Ru",
+    "Версия RussiaScript": "1.0",
+    "User-agent": "Mozilla/5.0"
+  }
+  SessionRS = JSON.stringify(SessionRussiaScript)
+  OperationSystems = [
+    "Unix",
+    "MacOS",
+    "BSD",
+    "Linux",
+    "Android",
+    "HarmonyOS",
+    "ios",
+    "ipadOS",
+    "tvOS"
+  ]
+  SessionRS.["ИмяПрограммы"] = RK.progname
+  SessionRS.["Язык"] = RK.lang
+  SessionRS.["Версия RussiaScript"] = "1.0"
+  UserAgent = fetch('https://httpbin.org/user-agent', {  
+        method: 'GET',  
+        headers: { 
+          "Content-Type": "application/json",  
+        }
+      })
+  SessionRS.["User-agent"] = UserAgent.["user-agent"]
+  OperationSystem = RK.os
+  OperationSystem = fetch(`https://shaman2016scratch.github.io/os?os="${OperationSystem}"`, {  
+        method: 'GET',  
+        headers: { 
+          "Content-Type": "application/json",  
+        }
+      })
+  SessionRS.["ОС"] = OperationSystem
+  SessionRussiaScript = JSON.parse(SessionRS)
+}
+function runRussiaScript(code) {
+  DataTypes = [
+    "text",
+    "math",
+    "boolean",
+    "peremens",
+    "block"
+  ]
+  RussiaScriptCommands = [
+    "Очистить Вывод",
+    "http запрос",
+    "Вывести",
+    "json",
+    "dom",
+    "Задать переменной значение",
+    "Импортировать расширение",
+    "Получить значение переменной"
+  ]
+  LibsRussiaScript = code.libs
+  codeRussiaScript = code.code
+  RunKey = code.RunKey
+  SessionRussiaScript(RunKey)
+  Peremens = {}
+  i = 0
+  i2 = 'true'
+  while (i2) {
+    if (codeRussiaScript.length < i) {
+      i2 = (false)
+    } else {
+      i2 = (true)
+    }
+    i++
+    i3 = codeRussiaScript.[i]
+    i4 = i3.command
+    i5 = i4.params
+    if (i4 == 'Очистить Вывод') {
+      RussiaScriptOutput = []
+    }
+    if (i4 == 'http запрос') {
+      HttpZapros = [
+        "Post",
+        "Delete",
+        "Get"
+      ]
+      fetch(i5.url, i5.params)
+    }
+    if (i4 == 'Вывести') {
+      RussiaScriptOutput.push(RussiaScriptGetValue(i5["Текст вывода"]))
+    }
+    if (i4 == 'json') {
+      RSjsonData = {
+        "Operations": [
+          "в Json-строку",
+          "в JavaScript обьект",
+          "Пустой объект",
+          "Пустой Массив",
+          "Задать ключ в объекте",
+          "Добавить элемент в начало массива",
+          "Добавить элемент в массив",
+          "Добавить элемент в массив в место",
+          "Удалить из объекта",
+          "Удалить последний элемент массива",
+          "Удалить первый элемент массива",
+          "Удалить элемент массива",
+          "Получить элемент массива",
+          "Получить элемент объекта"
+        ]
+      }
+    }
+    if (i4 == 'dom') {
+      RsDomData = {
+        "Operations": [
+          "getElement",
+          "innerHTML",
+          "TextContent",
+          "GetValue",
+          "NewElement"
+        ]
+      }
+    }
+    if (i4 == 'Задать переменной значение') {
+      peremens[i5.per] = i5.value
+    }
+    if (i4 == 'Импортировать расширение') {
+      console.error("RussiaScript Error: Import, Extensions (Libraries) not support!")
+    }
+  }
+}
+function RunRsCodeFromUrl(url) {
+  getCode = fetch(url, {  
+    method: 'GET',  
+    headers: { 
+      "Content-Type": "application/json",  
+    }
+  })
+  runRussiaScript(getCode)
 }
