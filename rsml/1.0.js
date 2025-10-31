6098f53964063abcef4bfe5265499e35d3920e41
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-10-31 15:39:28 +0300

    Update 1.0.js

--- rsml/1.0.js
+++ rsml/1.0.js
@@ -1,4 +1,24 @@
 // rsml (RSML) — RussiaScript Markup Language
 // rml/rsml
 rsmlDocument = []
-function rsml(rsml, htmObject) {}
+RSML = {
+  "command": ""
+}
+function rsml(rsml, htmObject) {
+  RSML.code = rsml
+  rsmlDocument = rsml
+  RSML.i = 0
+  RSML.i2 = 'true'
+  while (RSML.i2) {
+    if (RSML.code.length < RSML.i) {
+      RSML.i2 = (false)
+    } else {
+      RSML.i2 = (true)
+    }
+    RSML.i++
+    RSML.i3 = RSML.code.[RSML.i]
+    RSML.i4 = RSML.i3.tag
+    RSML.i5 = RSML.i3.params
+    RSML.i6 = RSML.i3.cod
+  }
+}
