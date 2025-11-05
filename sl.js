c2275e99fb97ecf38c26b8e1ed1eed9e0f2ba580
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-05 13:37:46 +0300

    Update sl.js

--- sl.js
+++ sl.js
@@ -2,3 +2,8 @@ const url = new URL(window.location)
 const searchParams = url.searchParams
 docData.txt = document.getElementById('lib')
 docData.lib = searchParams.get('lib')
+if (docData.lib === 'server') {
+  lib = `
+    
+  `
+}
