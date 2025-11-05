a83988fa2b407d6bb82d186bb07993cf196ef611
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-05 13:47:18 +0300

    Update sl.js

--- sl.js
+++ sl.js
@@ -4,6 +4,29 @@ docData.txt = document.getElementById('lib')
 docData.lib = searchParams.get('lib')
 if (docData.lib === 'server') {
   lib = `
-    
+    ExtLibRS = {
+      "version": "1.0",
+      "name": "Terminal RussiaScript+",
+      "id": "TerminalRS_sl",
+      "docs": "not",
+      "created by": "SHAMAN2016",
+    }
+    Ext_exampleExt = {
+      "data": ExtLibRS,
+      "commands": {
+        "set lang": "function (args) { sys.lang = args }",
+        "set os": "function (args) { sys.os = args }",
+      },
+      "commandList": [
+        "set lang",
+        "set os",
+      ],
+      "reporters": {
+        "get info": "function(args) { if (args === '1.0') { return } }"
+      },
+      "reportersList": [
+        "reporter"
+      ],
+    }
   `
 }
