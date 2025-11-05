5c2f994ea662f5b090d8c849021521f2ec15e2ec
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-05 11:34:35 +0300

    Update 1.0.js

--- rsml/1.0.js
+++ rsml/1.0.js
@@ -11,15 +11,9 @@ function rsml(rsml, htmlObject) {
   RSML.code = rsml
   rsmlDocument = rsml
   RSML.i = 0
-  RSML.i2 = 'true'
-  while (RSML.i2) {
-    if (RSML.code.length < RSML.i) {
-      RSML.i2 = (false)
-    } else {
-      RSML.i2 = (true)
-    }
-    RSML.i++
-    RSML.i3 = RSML.code.[RSML.i]
+  RSML.i2 = 'уже не нужен'
+  for (RSML.i = 0; RSML.i < RSML.code.length; RSML.i++)
+    RSML.i3 = RSML.code[RSML.i]
     RSML.i4 = RSML.i3.tag
     RSML.i5 = RSML.i3.params
     RSML.i6 = RSML.i3.cod
