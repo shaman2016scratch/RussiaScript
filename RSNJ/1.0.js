84e94f2f34a40e132292005cf404dea57e79a457
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-08 20:39:43 +0300

    Update 1.0.js

--- RSNJ/1.0.js
+++ RSNJ/1.0.js
@@ -1,7 +1,15 @@
 // RSNJ — RussiaScript not Json
 // RSNJ — тот же RussiaScript но не в формате Json а в формате обычного языка программирования
 RSNJ = {}
+function RsnjGetValue(t) {
+  if (typeof t == 'string' || typeof t == 'number' || typeof t == 'object' || typeof t == 'boolean') {
+    return t
+  }
+}
 function RunRSNJ(c) {
   RSNJ.code = c
   RSNJ.code = RSNJ.code.split("\n")
+  for(RSNJ.i = 0; i < RSNJ.code.length; RSNJ.i++) {
+    RSNJ.i2 = RSNJ.code[RSNJ.i]
+  }
 }
