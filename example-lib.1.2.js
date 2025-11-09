f740c59f36485f5309299fa037dc06f8bbc73dce
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 11:54:56 +0300

    Update example-lib.1.2.js

--- example-lib.1.2.js
+++ example-lib.1.2.js
@@ -1,1 +1,31 @@
-
+ExtLibRS = {
+  "version": "1.0",
+  "name": "Example Extension",
+  "id": "exampleExt",
+  "docs": "example.com",
+  "created by": "ExampleUser12345",
+  "rs v": "1.2", 
+}
+Ext_exampleExt = {
+  "data": ExtLibRS,
+  "commands": {
+    "command": "function\(args\) \{ alert(args) \}"
+  },
+  "commandList": [
+    "command"
+  ],
+  "reporters": {
+    "reporter": "function\(args\) \{ return args.return \}"
+  },
+  "reportersList": [
+    "reporter"
+  ],
+}
+fromed_exampleExt = {
+  "fromModule": {
+    "src": "@Shaman2016Scratch/RussiaScirpt:main/fromsModule/1.2/1.json", /* должно находиться в списке доступных src */
+    "type": "txt/json", /* выбор из txt/json, txt/python и txt/javascript */
+  },
+  "fromes": {},
+  "fromeList": [],
+}
