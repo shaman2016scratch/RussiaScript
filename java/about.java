b42be7332782a037042983c722e194bc39f81384
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 14:54:14 +0300

    Update about.java

--- java/about.java
+++ java/about.java
@@ -1,8 +1,14 @@
+import java.util.Scanner;
 public class RussiaScript
 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     System.out.println("О RussiaScript");
-    System.out.println("RussiaScript — язык программирования на JavaScript");
+    System.out.println("Ваше имя");
+    String name = sc.nextLine();
+    System.out.println(name);
+    System.out.println("Привет, "+name+"!")
+    System.out.println("RussiaScript — это язык программирования на JavaScript");
     System.out.println("Также планируются версии RussiaScript для Node.js, python, C, C# и Java");
     System.out.println("Документация: https://shaman2016scratch.github.io/Docs/RussiaScript");
     System.out.println("Статья на SHAMAN2016-WIKI: https://shaman2016scratch.github.io/shaman2016.github.io/wiki/RussiaScript");
