2d22b1cb737cd9e650cf73402097a858244baeb9
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 13:32:24 +0300

    Update 1.2.java

--- java/1.2.java
+++ java/1.2.java
@@ -1,7 +1,7 @@
 public class RussiaScript
 {
   public static void main(String[] args) {
-    String version;
+    String version = "1.2";
     String sys;
     String user;
     System.out.println("RussiaScript 1.2 For Java!");
