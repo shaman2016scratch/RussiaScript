dd90489fd687ce2185963253064d81526ab36460
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 13:24:11 +0300

    Update and rename 1.2.java to 1.2.java

--- java/1.2.java
+++ java/1.2.java
@@ -1,6 +1,9 @@
 public class RussiaScript
 {
   public static void main(String[] args) {
+    String version;
+    String sys;
+    String user;
     System.out.println("RussiaScript 1.2 For Java!");
   }
 }
