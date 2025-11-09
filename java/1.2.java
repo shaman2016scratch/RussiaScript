65ac5d37e2450f72842f1571ed77b4b02ccc5fd7
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 14:39:05 +0300

    Update 1.2.java

--- java/1.2.java
+++ java/1.2.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 public class RussiaScript
 {
   public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
     String version = "1.2";
     String sys;
     String user;
