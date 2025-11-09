5db872c92753e794263e20cf6d30b7cbeaa01a06
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 12:10:12 +0300

    Update 1.py

--- fromsModule/1.2/1.py
+++ fromsModule/1.2/1.py
@@ -1,1 +1,10 @@
-
+from 1.2 import RussiaScript
+from Runner import JavaScript
+from 1.2 import RussiaScriptLibs
+def main():
+  js('console.log(\'started\')')
+  RussiaScriptLibs.v1-2.object({
+    "type": "python",
+    "v": "1.0",
+    "rs v": "1.2",
+  })
