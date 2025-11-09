a59b73a0f5471fbd358a702ec49bcda0582d675b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 12:20:22 +0300

    Update 1.py

--- fromsModule/1.2/1.py
+++ fromsModule/1.2/1.py
@@ -7,4 +7,19 @@ def main():
     "type": "python",
     "v": "1.0",
     "rs v": "1.2",
+    "name": "PythonRsLibs1",
+  })
+  RussiaScriptLibs.v1-2.object.rs_v(RussiaScript.v1-2.terminal('get RussiaScript version'))
+  RussiaScriptLibs.v1-2.object.v('1.0')
+  RussiaScriptLibs.v1-2.object.type('python')
+  RussiaScriptLibs.v1-2.object.name('PythonRsLibs1')
+  RussiaScriptLibs.v1-2.object.exampleFrom({
+    "from1": {
+      "reporters": ["reporter1"],
+      "commands": ["command1"],
+    },
+    "from2": {
+      "reporters": ["reporter2"],
+      "commands": ["command2"],
+    },
   })
