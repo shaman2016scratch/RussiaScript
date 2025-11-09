7563c668088b36cb351a36b3f95e267066d62a16
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-09 14:47:58 +0300

    Create about.java

---
+++ java/about.java
@@ -0,0 +1,10 @@
+public class RussiaScript
+{
+  public static void main(String[] args) {
+    System.out.println("О RussiaScript");
+    System.out.println("RussiaScript — язык программирования на JavaScript");
+    System.out.println("Также планируются версии RussiaScript для Node.js, python, C, C# и Java");
+    System.out.println("Документация: https://shaman2016scratch.github.io/Docs/RussiaScript");
+    System.out.println("Статья на SHAMAN2016-WIKI: https://shaman2016scratch.github.io/shaman2016.github.io/wiki/RussiaScript");
+  }
+}
