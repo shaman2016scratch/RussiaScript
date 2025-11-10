bb5de14952cc4b0821ec90b575c7efd55a1c600b
Shaman2016 scratcher <g.ilkh@yandex.ru>
2025-11-10 17:22:27 +0300

    Create exampleCustomTerminal.1.2.js

---
+++ exampleCustomTerminal.1.2.js
@@ -0,0 +1,12 @@
+TerminalLibInfo = {
+  "id": "LibId",
+  "version": "1.0",
+  "created": "Example user",
+  "github": "example/test",
+  "webSite": "example.com",
+  "versionsUrl": "github.com/example/test/"
+}
+LibId_commands = {
+  "list": [],
+  "commands": {},
+}
